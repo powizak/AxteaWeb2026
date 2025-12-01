20596629ad956fe3164e85c3d54c48989ab804c9
powizak <powizak@gmail.com>
2025-12-02 00:19:28 +0100

    deleted some obsolete files and comments in index

--- index.php
+++ index.php
@@ -18,9 +18,6 @@
     <!-- Font Awesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     
-    <!-- Google Fonts (Removed) -->
-    <!-- <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600;700&family=Playfair+Display:wght@700&display=swap" rel="stylesheet"> -->
-
     <!-- 1. GOOGLE CONSENT MODE V2 -->
     <script>
         window.dataLayer = window.dataLayer || [];
